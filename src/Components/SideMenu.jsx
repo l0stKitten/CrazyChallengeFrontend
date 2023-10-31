3a9a6d4f6acbc746b3f279e428ec2055c9ff8215
daria <dlopezf@ulasalle.edu.pe>
2023-10-31 00:18:49 -0500

    SideBar and AppBar

--- src/Components/SideMenu.jsx
+++ src/Components/SideMenu.jsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { styled, useTheme } from '@mui/material/styles';
+import AppBarCC from './AppBarCC'
+
 import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
-import MuiAppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import CssBaseline from '@mui/material/CssBaseline';
 import Typography from '@mui/material/Typography';
@@ -16,8 +16,12 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import HomeOutlined from '@mui/icons-material/HomeOutlined';
+import ChatOutlined from '@mui/icons-material/ChatOutlined';
+import AccountBoxOutlinedIcon from '@mui/icons-material/AccountBoxOutlined';
+import MilitaryTechOutlinedIcon from '@mui/icons-material/MilitaryTechOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import AdbIcon from '@mui/icons-material/Adb';
 
 const drawerWidth = 240;
 
@@ -51,24 +55,6 @@ const DrawerHeader = styled('div')(({ theme }) => ({
 	...theme.mixins.toolbar,
 }));
 
-const AppBar = styled(MuiAppBar, {
-	shouldForwardProp: (prop) => prop !== 'open',
-	})(({ theme, open }) => ({
-		zIndex: theme.zIndex.drawer + 1,
-		transition: theme.transitions.create(['width', 'margin'], {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.leavingScreen,
-		}),
-		...(open && {
-			marginLeft: drawerWidth,
-			width: `calc(100% - ${drawerWidth}px)`,
-			transition: theme.transitions.create(['width', 'margin'], {
-			easing: theme.transitions.easing.sharp,
-			duration: theme.transitions.duration.enteringScreen,
-			}),
-	}),
-}));
-
 const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
 	({ theme, open }) => ({
 			width: drawerWidth,
@@ -98,84 +84,93 @@ export default function SideMenu() {
 		setOpen(false);
 	};
 
+	const icons = [
+		<HomeOutlined/>,
+		<ChatOutlined/>,
+		<AccountBoxOutlinedIcon/>,
+		<MilitaryTechOutlinedIcon/>,
+	];
+
 	return (
 		<Box sx={{ display: 'flex' }}>
 		<CssBaseline />
-		<AppBar position="fixed" open={open}>
-			<Toolbar>
-			<IconButton
-				color="inherit"
-				aria-label="open drawer"
-				onClick={handleDrawerOpen}
-				edge="start"
-				sx={{
-				marginRight: 5,
-				...(open && { display: 'none' }),
-				}}
-			>
-				<MenuIcon />
-			</IconButton>
-			<Typography variant="h6" noWrap component="div">
-				Mini variant drawer
-			</Typography>
-			</Toolbar>
-		</AppBar>
+		<AppBarCC openVar={open} handleDrawerOpen={handleDrawerOpen}/>
 		<Drawer variant="permanent" open={open}>
 			<DrawerHeader>
-			<IconButton onClick={handleDrawerClose}>
-				{theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-			</IconButton>
+				<AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+				<Typography
+					variant="h6"
+					noWrap
+					component="a"
+					href="#app-bar-with-responsive-menu"
+					sx={{
+					mr: 2,
+					display: { xs: 'none', md: 'flex' },
+					fontFamily: 'monospace',
+					fontWeight: 700,
+					letterSpacing: '.3rem',
+					color: 'inherit',
+					textDecoration: 'none',
+					}}
+				>
+					Crazy Challenge
+				</Typography>
+				<IconButton onClick={handleDrawerClose}>
+					{theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+				</IconButton>
 			</DrawerHeader>
-			<Divider />
-			<List>
-			{['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-				<ListItem key={text} disablePadding sx={{ display: 'block' }}>
-				<ListItemButton
+	
+			<List sx={{ flexGrow: 1 }}>
+				{['Inicio', 'Conversaciones', 'Perfil', 'Rangos'].map((text, index) => (
+					<ListItem key={text} disablePadding sx={{ display: 'block' }}>
+					<ListItemButton
+						sx={{
+						minHeight: 48,
+						justifyContent: open ? 'initial' : 'center',
+						px: 2.5,
+						}}
+					>
+						<ListItemIcon
+						sx={{
+							minWidth: 0,
+							mr: open ? 3 : 'auto',
+							justifyContent: 'center',
+						}}
+						>
+						{icons[index]}
+						</ListItemIcon>
+						<ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+					</ListItemButton>
+					</ListItem>
+				))}
+
+				<ListItem disablePadding
 					sx={{
-					minHeight: 48,
-					justifyContent: open ? 'initial' : 'center',
-					px: 2.5,
-					}}
-				>
-					<ListItemIcon
-					sx={{
-						minWidth: 0,
-						mr: open ? 3 : 'auto',
-						justifyContent: 'center',
+						position: 'fixed',
+						bottom: 0,
 					}}
 					>
-					{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-					</ListItemIcon>
-					<ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-				</ListItemButton>
-				</ListItem>
-			))}
-			</List>
-			<Divider />
-			<List>
-			{['All mail', 'Trash', 'Spam'].map((text, index) => (
-				<ListItem key={text} disablePadding sx={{ display: 'block' }}>
-				<ListItemButton
-					sx={{
-					minHeight: 48,
-					justifyContent: open ? 'initial' : 'center',
-					px: 2.5,
-					}}
-				>
-					<ListItemIcon
-					sx={{
-						minWidth: 0,
-						mr: open ? 3 : 'auto',
-						justifyContent: 'center',
-					}}
+					<ListItemButton sx={{
+						minHeight: 48,
+						justifyContent: open ? 'center' : 'center',
+						px: 2.5,
+						}}
 					>
-					{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-					</ListItemIcon>
-					<ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-				</ListItemButton>
+						<ListItemIcon sx={{
+							minWidth: 0,
+							mr: open ? 3 : 'auto',
+							justifyContent: 'center',
+						}}
+						>
+						<DarkModeOutlinedIcon />
+						</ListItemIcon>
+						<ListItemText primary="Modo Oscuro" sx={{ opacity: open ? 1 : 0 }} />
+					</ListItemButton>
+					
 				</ListItem>
-			))}
+
 			</List>
+			
 		</Drawer>
 		<Box component="main" sx={{ flexGrow: 1, p: 3 }}>
 			<DrawerHeader />
