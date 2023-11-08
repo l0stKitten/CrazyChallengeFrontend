517cc8b32a191c36d1e516b5e15f9a2048301bc4
Daria Lopez <dlopezf@ulasalle.edu.pe>
2023-11-07 20:12:24 -0500

    Lottiefile links

--- src/Components/Reaction.jsx
+++ src/Components/Reaction.jsx
@@ -1,5 +1,12 @@
 import React, {Fragment, useEffect} from 'react'
 
 export default function Reaction() {
+    //https://lottiefiles.com/animations/all-emoji-oNvHHn8ZKy
+    //https://lottiefiles.com/animations/emojis-love-kYtnx3yysts
+    //https://lottiefiles.com/animations/banded-face-emoji-y07r7gTcHw
+
+    return (
+        <div></div>
+    );
 
 }
